a32608d98e28a335e5a6c40d7d6571de63b9424b
DVDAndroid <davide.intelligent@gmail.com>
2015-10-27 21:39:00 +0100

    first commit

---
+++ PrintScreenUtility/Main.Designer.cs
@@ -0,0 +1,133 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Print_Screen_Utility
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.cmsNotifiy = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.showToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.save = new System.Windows.Forms.Button();
+            this.previewScreenshot = new System.Windows.Forms.PictureBox();
+            this.cmsNotifiy.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.previewScreenshot)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon.BalloonTipText = "Clich here to save the screenshot";
+            this.notifyIcon.BalloonTipTitle = "Screen captured";
+            this.notifyIcon.ContextMenuStrip = this.cmsNotifiy;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Print Screen Utility";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.BalloonTipClicked += new System.EventHandler(this.ballon_Click);
+            this.notifyIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseDoubleClick);
+            // 
+            // cmsNotifiy
+            // 
+            this.cmsNotifiy.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.showToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.cmsNotifiy.Name = "cmsNotifiy";
+            this.cmsNotifiy.Size = new System.Drawing.Size(104, 48);
+            // 
+            // showToolStripMenuItem
+            // 
+            this.showToolStripMenuItem.Name = "showToolStripMenuItem";
+            this.showToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.showToolStripMenuItem.Text = "Show";
+            this.showToolStripMenuItem.Click += new System.EventHandler(this.showToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // save
+            // 
+            this.save.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.save.Image = global::Print_Screen_Utility.Properties.Resources.ic_save_grey600_18dp;
+            this.save.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.save.Location = new System.Drawing.Point(395, 483);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(114, 74);
+            this.save.TabIndex = 5;
+            this.save.Text = "Save";
+            this.save.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.saveButton_click);
+            // 
+            // previewScreenshot
+            // 
+            this.previewScreenshot.Location = new System.Drawing.Point(12, 12);
+            this.previewScreenshot.Name = "previewScreenshot";
+            this.previewScreenshot.Size = new System.Drawing.Size(331, 186);
+            this.previewScreenshot.TabIndex = 4;
+            this.previewScreenshot.TabStop = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(702, 581);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.previewScreenshot);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "Print Screen Utitity";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.cmsNotifiy.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.previewScreenshot)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip cmsNotifiy;
+        private System.Windows.Forms.ToolStripMenuItem showToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.PictureBox previewScreenshot;
+        private System.Windows.Forms.Button save;
+                
+    }
+}
+
---
+++ PrintScreenUtility/Main.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace Print_Screen_Utility
+{
+
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.Height = GetHeight(80);
+            this.Width = GetWidth(61) + 24;
+            this.MaximizeBox = false;
+
+            save.Location = new System.Drawing.Point((GetWidth(60) / 2) + 12 - 57, (GetHeight(63)));
+            notifyIcon.Visible = true;
+            previewScreenshot.Height = GetHeight(60);
+            previewScreenshot.Width = GetWidth(60);
+            save.Enabled = img != null;
+        }
+
+        private void showToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            gHook.unhook();
+            Environment.Exit(0);
+        }
+
+        private void notifyIcon_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+
+        private void Form1_FormClosing(object s, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+            this.Hide();
+        }
+
+        GlobalKeyboardHook gHook;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gHook = new GlobalKeyboardHook();
+            gHook.hook();
+            gHook.KeyDown += new KeyEventHandler(gHook_KeyDown);
+            gHook.HookedKeys.Add(Keys.PrintScreen);
+        }
+
+        private void gHook_KeyDown(object sender, KeyEventArgs e)
+        {
+            notifyIcon.ShowBalloonTip(20);
+        }
+
+        Image img;
+        private void ballon_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            if (Clipboard.ContainsImage())
+            {
+                img = Clipboard.GetImage();
+                previewScreenshot.SizeMode = PictureBoxSizeMode.StretchImage;
+                previewScreenshot.Image = img;
+            }
+            else
+            {
+                MessageBox.Show("Cannot get image", "Cannot get image", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            save.Enabled = img != null;
+        }
+
+        private void saveButton_click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.FileName = "Screenshot";
+            dialog.AddExtension = true;
+            dialog.OverwritePrompt = true;
+            dialog.Filter = "PNG|*.png|GIF|*.gif|BMP|*.bmp|JPEG|*.jpg;*.jpeg";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                img.Save(dialog.FileName);
+            }
+        }
+
+        public Rectangle GetScreen()
+        {
+            return Screen.FromControl(this).Bounds;
+        }
+
+        public int GetWidth(int p)
+        {
+            return (GetScreen().Width * p) / 100;
+        }
+
+        public int GetHeight(int p)
+        {
+            return (GetScreen().Height * p) / 100;
+        }
+    }
+}
